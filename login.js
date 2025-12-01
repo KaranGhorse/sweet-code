6aa648312d98a37f24b1f1846307faf8a3c8e387
Karan Ghorse <118005333+KaranGhorse@users.noreply.github.com>
2025-12-01 21:08:37 +0530

    initial

---
+++ login.js
@@ -0,0 +1,13 @@
+// const btn = document.getElementById("themeBtn");
+// const gitlogo = document.getElementById("github-logo");
+
+// btn.addEventListener("click", () => {
+//   document.body.classList.toggle("dark");
+
+//   if (document.body.classList.contains("dark")) {
+//     gitlogo.src = "./assets/githublogo.png"; 
+//     // Dark mode logo
+//   } else {
+//     gitlogo.src = "./assets/googlelogo.png";  // Light mode logo
+//   }
+// });
---
+++ script.js
@@ -0,0 +1,63 @@
+const btn = document.getElementById("themeBtn");
+const logo = document.getElementById("logo");
+const gitlogo = document.getElementById("github-logo");
+
+
+btn.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+  
+
+  if (document.body.classList.contains("dark")) {
+    logo.src = "./assets/logo-dark.png"; 
+    gitlogo.src = "./assets/githublogodark.png"; 
+    gitlogo.style.width="20px"; 
+    gitlogo.style.marginRight="8px"; 
+    // Dark mode logo(black)
+  } else {
+    logo.src = "./assets/logo-bright.png"; 
+    gitlogo.src = "./assets/githublogo.png";
+    gitlogo.style.width="40px"; 
+    // Light mode logo (white)
+  }
+});
+
+
+const wBtn = document.getElementById("widthBtn");
+const profilelarea = document.getElementById("profileLeftArea");
+const profilerarea = document.getElementById("profileRightArea");
+// const widthArrow = document.getElementById("widthArrow");
+wBtn.addEventListener("click", () => {
+    document.body.classList.toggle("profile");
+    
+  if (document.body.classList.contains("profile")) {
+    profilelarea.style.width="8%";
+    profilerarea.style.width="100%";
+   
+  }
+
+  else{
+    profilelarea.style.width="260px";
+  }
+
+});
+
+const eBtn = document.getElementById("editBTN");
+// const editarea = document.getElementById("edit-sectionid");
+eBtn.addEventListener("click",()=>{
+     document.body.classList.toggle("show");
+  // document.editarea.classList.toggle.add("show");
+
+})
+
+const eCross = document.getElementById("edit-cross");
+const editarea = document.getElementById("edit-sectionid");
+const user = document.querySelectorAll(".profile-section")
+eCross.addEventListener("click",()=>{
+       document.body.classList.toggle("show");
+})
+
+
+
+
+
+
